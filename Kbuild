9df6bfb5df9d58be79ee7c80d2b0b27bdeb83c95
Brendan Jackman <jackmanb@google.com>
2023-12-21 16:27:45 +0000

    init

---
+++ Kbuild
@@ -0,0 +1,2 @@
+obj-m += page_alloc_bench.o
+page_alloc_bench-y := page_alloc_bench.kmod.o
---
+++ page_alloc_bench.kmod.c
@@ -0,0 +1,10 @@
+#include <linux/module.h>
+
+static int __init page_alloc_bench_init(void)
+{
+	printk("hello world\n");
+	return 0;
+}
+module_init(page_alloc_bench_init);
+
+MODULE_LICENSE("gpl");
