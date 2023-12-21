582d29e83913c17ffa0745b4d2e91966de45374b
Brendan Jackman <jackmanb@google.com>
2023-12-21 16:54:27 +0000

    Add module_exit
    Turns out you need this if you have module _init

--- page_alloc_bench.kmod.c
+++ page_alloc_bench.kmod.c
@@ -7,4 +7,11 @@ static int __init page_alloc_bench_init(void)
 }
 module_init(page_alloc_bench_init);
 
+static void __exit page_alloc_bench_exit(void)
+{
+	printk("good buy world\n");
+	return;
+}
+module_exit(page_alloc_bench_exit);
+
 MODULE_LICENSE("GPL");
