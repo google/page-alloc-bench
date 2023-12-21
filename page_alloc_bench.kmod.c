0b23f2f1753b6149c19520fa248e6f5f7cc72057
Brendan Jackman <jackmanb@google.com>
2023-12-21 17:01:08 +0000

    Register chardev

--- page_alloc_bench.kmod.c
+++ page_alloc_bench.kmod.c
@@ -1,15 +1,27 @@
+#include <linux/cdev.h>
+#include <linux/fs.h>
 #include <linux/module.h>
+
+dev_t dev;
+const int minor_devnum = 1;
 
 static int __init page_alloc_bench_init(void)
 {
-	printk("hello world\n");
+	int err;
+
+	err = alloc_chrdev_region(&dev, 0, minor_devnum, "page_alloc_bench");
+	if (err) {
+		printk("alloc_chardev_region: %d\n", err);
+		return err;
+	}
+
 	return 0;
 }
 module_init(page_alloc_bench_init);
 
 static void __exit page_alloc_bench_exit(void)
 {
-	printk("good buy world\n");
+	unregister_chrdev_region(dev, minor_devnum);
 	return;
 }
 module_exit(page_alloc_bench_exit);
