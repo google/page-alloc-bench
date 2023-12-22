925d9824c210a576646ee4b4b4ffc8ae009bfa7b
Brendan Jackman <jackmanb@google.com>
2023-12-22 16:28:56 +0000

    Add simple workload

--- page_alloc_bench.go
+++ page_alloc_bench.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"os"
 	"runtime"
 	"slices"
 	"sync"
+	"sync/atomic"
 	"syscall"
 	"time"
 	"unsafe"
@@ -72,6 +74,59 @@ func (k *kmod) freePage(page page) error {
 	return ioctl(k.File, PAB_IOCTL_FREE_PAGE, uintptr(page))
 }
 
+const (
+	kilobyte = 1024
+	megabyte = 1024 * kilobyte
+	gigabyte = 1024 * megabyte
+)
+
+type stats struct {
+	pagesAllocated atomic.Uint64 // Only incremented; subtract pagesFreed to count leaks.
+	pagesFreed     atomic.Uint64
+}
+
+func (s *stats) String() string {
+	return fmt.Sprintf("allocated=%d freed=%d", s.pagesAllocated.Load(), s.pagesFreed.Load())
+}
+
+var totalMemoryFlag = flag.Uint64("total-memory", 256*megabyte, "Total memory to allocate in bytes")
+
+// A workload that allocates and the frees a bunch of pages on every CPU.
+type workload struct {
+	kmod        *kmod
+	stats       *stats
+	pagesPerCPU uint64
+}
+
+// per-CPU element of a workload. Assumes that the calling goroutine is already
+// pinned to an appropriate CPU.
+func (w *workload) runCPU(ctx context.Context) error {
+	var pages []page
+
+	defer func() {
+		for _, page := range pages {
+			w.kmod.freePage(page)
+			w.stats.pagesFreed.Add(1)
+		}
+	}()
+
+	for i := uint64(0); i < w.pagesPerCPU; i++ {
+		if err := ctx.Err(); err != nil {
+			return err
+		}
+
+		page, err := w.kmod.allocPage()
+		if err != nil {
+			return fmt.Errorf("allocating page %d: %v", i, err)
+		}
+		w.stats.pagesAllocated.Add(1)
+
+		pages = append(pages, page)
+	}
+
+	return nil
+}
+
 func doMain() error {
 	file, err := os.Open("/proc/page_alloc_bench")
 	if err != nil {
@@ -80,10 +135,18 @@ func doMain() error {
 	kmod := kmod{file}
 	defer kmod.Close()
 
+	var stats stats
+
+	workload := workload{
+		kmod:        &kmod,
+		stats:       &stats,
+		pagesPerCPU: (*totalMemoryFlag / uint64(os.Getpagesize())) / uint64(runtime.NumCPU()),
+	}
+	fmt.Printf("Started %d threads, each allocating %d pages\n", runtime.NumCPU(), workload.pagesPerCPU)
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	// The WaitGroup + errCh is a poor-man's
-	// https://pkg.go.dev/golang.org/x/sync/errgroup because that isn't in
+	// The WaitGroup + errCh is a poor-man's sync.ErrGroup, that isn't in
 	// the proper stdlib yet, and it doesn't seem worth throwing away the
 	// no-Go-dependencies thing for that.
 	var wg sync.WaitGroup
@@ -102,29 +165,37 @@ func doMain() error {
 				errCh <- fmt.Errorf("schedSetaffinity(%+v): %c", cpuMask, err)
 			}
 
-			<-ctx.Done()
+			err = workload.runCPU(ctx)
+			if err != nil {
+				errCh <- fmt.Errorf("workload failed on CPU %d: %v", cpu, err)
+			}
 			wg.Done()
 		}()
 	}
 
+	// Poor-man's sync.ErrGroup.Wait()
 	done := make(chan any)
 	go func() {
 		wg.Wait()
 		close(done)
 	}()
-
 	select {
 	case <-done:
-		return nil
+		break
 	case err := <-errCh:
 		return err
 	}
+
+	fmt.Printf("stats: %s", workload.stats.String())
+
+	return nil
 }
 
 func main() {
+	flag.Parse()
+
 	if err := doMain(); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
-	fmt.Printf("Done")
 }
--- run.sh
+++ run.sh
@@ -3,10 +3,9 @@
 set -eu
 
 insmod page_alloc_bench.ko
-
 do_rmmod() {
         rmmod page_alloc_bench.ko
 }
-
 trap do_rmmod EXIT
-./page_alloc_bench
+
+./page_alloc_bench "$@"
