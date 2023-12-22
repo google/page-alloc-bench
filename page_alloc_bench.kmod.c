4a4e6548859db74921102e12d00e1a2f063e2b7c
Brendan Jackman <jackmanb@google.com>
2023-12-22 11:25:36 +0000

    Switch to procfs

--- page_alloc_bench.kmod.c
+++ page_alloc_bench.kmod.c
@@ -2,44 +2,33 @@
 #include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/module.h>
+#include <linux/proc_fs.h>
 
 #define NAME "page_alloc_bench"
 
-dev_t dev;
-const int minor_devnum = 1;
-static struct cdev cdev;
+static struct proc_dir_entry *procfs_file;
 
-static struct file_operations fops = {};
+static long page_alloc_bench_ioctl(struct file *filp, unsigned int request, unsigned long argp) {
+	return 0;
+}
+
+static struct proc_ops proc_ops = {
+	.proc_ioctl = page_alloc_bench_ioctl,
+	.proc_open = nonseekable_open,
+	.proc_lseek = no_llseek,
+};
 
 static int __init page_alloc_bench_init(void)
 {
-	int err;
-
-	err = alloc_chrdev_region(&dev, 0, minor_devnum, NAME);
-	if (err) {
-		printk("alloc_chardev_region: %d\n", err);
-		return err;
-	}
-
-	cdev_init(&cdev, &fops);
-	err = cdev_add(&cdev, dev, minor_devnum);
-	if (err)
-		goto error;
+	procfs_file = proc_create(NAME, 0, NULL, &proc_ops);
 
 	return 0;
-
-error:
-	if (dev)
-		unregister_chrdev_region(dev, minor_devnum);
-	return err;
 }
 module_init(page_alloc_bench_init);
 
 static void __exit page_alloc_bench_exit(void)
 {
-	cdev_del(&cdev);
-	unregister_chrdev_region(dev, minor_devnum);
-	return;
+	proc_remove(procfs_file);
 }
 module_exit(page_alloc_bench_exit);
 
