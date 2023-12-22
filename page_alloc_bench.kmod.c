70acd017f737944597f3ef12b224a715d703dfd1
Brendan Jackman <jackmanb@google.com>
2023-12-22 12:44:37 +0000

    Untested ioctl code

--- page_alloc_bench.kmod.c
+++ page_alloc_bench.kmod.c
@@ -3,13 +3,40 @@
 #include <linux/fs.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
+#include <linux/uaccess.h>
 
-#define NAME "pab"
+#define NAME "page_alloc_bench"
 
-static long pab_ioctl(struct file *filp, unsigned int request, unsigned long argp) {
-	return 0;
+#define PAB_IOCTL 'P'
+#define PAB_IOCTL_ALLOC_PAGE	_IOR(PAB_IOCTL, 1, struct page *)
+#define PAB_IOCTL_FREE_PAGE 	_IOW(PAB_IOCTL, 1, struct page *)
+
+static long pab_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
+	switch (cmd) {
+
+		case PAB_IOCTL_ALLOC_PAGE: {
+			struct page *page;
+
+			page = alloc_page(GFP_KERNEL);
+
+			if (!page)
+				return -ENOMEM;
+
+			return put_user(page, (struct page **)arg);
+		}
+		case PAB_IOCTL_FREE_PAGE: {
+			struct page *page = (struct page *)arg;
+
+			__free_page(page);
+			return 0;
+		}
+		default:
+			pr_err("Invalid page_alloc_bench ioctl 0x%x\n", cmd);
+			return -EINVAL;
+	}
 }
 
+/* Procfs is a pretty convenient way to create a file where we can receive ioctls. */
 static struct proc_ops proc_ops = {
 	.proc_ioctl = pab_ioctl,
 	.proc_open = nonseekable_open,
